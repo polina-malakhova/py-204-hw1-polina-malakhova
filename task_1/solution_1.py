d5340def667c7224b04da33e52c0d9e244760877
polina-malakhova <72040802+polina-malakhova@users.noreply.github.com>
2020-10-27 19:55:13 +0300

    Update solution_1.py

--- task_1/solution_1.py
+++ task_1/solution_1.py
@@ -1,1 +1,9 @@
+a=input("enter a number: ")
+b=-1                          #b=cамое большое число
+for i in a:
+	if int(i)>b:                 #если символ больше принятого сейчас СБЧ, то о становится СБЧ
+		b=int(i)
+
+print(b)
+
 # your code here
